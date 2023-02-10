39dfe5a792408bab0729f33e462716b9c041ae99
Kari <kouglas2@gmail.com>
2023-02-09 19:29:45 -0600

    update follow-along-nav

--- follow-along-nav/script.js
+++ follow-along-nav/script.js
@@ -1,6 +1,14 @@
-// const sayHi = ()=> {
-//     console.log('hey hoe')
-// }
+const triggers = document.querySelectorAll('.cool > li')
+const background = document.querySelectorAll('.dropdownBackground')
+const nav = document.querySelectorAll('.top')
 
-// sayHi()
+function handleEnter(){
+    console.log('enter')
+}
 
+function handleLeave(){
+    console.log('leave')
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter))
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave))
